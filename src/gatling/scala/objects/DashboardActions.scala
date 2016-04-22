10eec649a68fd1706c6cde6e09d9f969d0d7b925
smue <sebastian.mueller@itconcepts.net>
2016-04-22 15:12:55 +0200

    Simulation for apply role without approval

--- src/gatling/scala/objects/DashboardActions.scala
+++ src/gatling/scala/objects/DashboardActions.scala
@@ -8,6 +8,10 @@ import io.gatling.http.Predef._
   */
 class DashboardActions {
 
+  val RoleNameSearchCriteria = "IDM+Identity+Activation+Approver"
+
+  val sucessfullApply = "The data of the process were saved successfully"
+
   val headers_0 = Map(
     "Origin" -> "http://5.9.110.178")
 
@@ -38,4 +42,61 @@ class DashboardActions {
       .headers(headers_0)
       .formParam("aForm0_SUBMIT", "1")
       .formParam("aForm0%3A_link_hidden_", ""))
+
+  object RoleActions {
+
+    val searchForRoleByName =
+      exec(http("Dashboard - Apply for Role: Search Role by Name")
+      .post("/goIdentity/jsf/2/workflow-custom/Dashboard/ApplyForRoleSelfService_3501_1/ApplyForRoleSelfService_3501_1Init.jsp?form:searchRoles=Search")
+      .headers(headers_0)
+      .formParam("form%3AcnRoleCriteria", RoleNameSearchCriteria)
+      .formParam("form%3AdescriptionRoleCriteria", "")
+      .formParam("form%3AitcApprovalRequiredRoleCriteria", "")
+      .formParam("form%3AcriteriaRoleCategory", "")
+      .formParam("form_SUBMIT", "1")
+      .formParam("form%3A_link_hidden_", ""))
+
+     val chooseRole =
+      exec(http("Dashboard - Apply for Role: Choose Role")
+          .post("/goIdentity/jsf/2/workflow-custom/Dashboard/ApplyForRoleSelfService_3501_1/ApplyForRoleSelfService_3501_1Init.jsp?form:dt02_0:choose2=Choose")
+          .headers(headers_0)
+          .formParam("form%3AcnRoleCriteria", RoleNameSearchCriteria)
+          .formParam("form%3AdescriptionRoleCriteria", "")
+          .formParam("form%3AitcApprovalRequiredRoleCriteria", "")
+          .formParam("form%3AcriteriaRoleCategory", "")
+          .formParam("form%3APageChooserRole", "")
+          .formParam("form%3Adt02%3AsheaderCategory", "")
+          .formParam("form%3Adt02%3Ascn", "")
+          .formParam("form%3Adt02%3Asdescription", "")
+          .formParam("form%3Adt02%3AsitcApprovalRequired", "")
+          .formParam("form%3Adt02%3AsitcRoleResponsible", "")
+          .formParam("form%3AexpireDate", "")
+          .formParam("form_SUBMIT", "1")
+          .formParam("form%3A_link_hidden_", ""))
+
+     val enterDateAndNext =
+      exec(http("Dashboard - Apply for Role: Enter Date and Next")
+      .post("/goIdentity/jsf/2/workflow-custom/Dashboard/ApplyForRoleSelfService_3501_1/ApplyForRoleSelfService_3501_1Init.jsp?form:wizardApplyForRoleSelfService_next=Next")
+      .headers(headers_0)
+      .formParam("form%3AcnRoleCriteria", RoleNameSearchCriteria)
+      .formParam("form%3AdescriptionRoleCriteria", "")
+      .formParam("form%3AitcApprovalRequiredRoleCriteria", "")
+      .formParam("form%3AcriteriaRoleCategory", "")
+      .formParam("form%3APageChooserRole", "")
+      .formParam("form%3Adt02%3AsheaderCategory", "")
+      .formParam("form%3Adt02%3Ascn", "")
+      .formParam("form%3Adt02%3Asdescription", "")
+      .formParam("form%3Adt02%3AsitcApprovalRequired", "")
+      .formParam("form%3Adt02%3AsitcRoleResponsible", "")
+      .formParam("form%3AexpireDate", "4%2F22%2F17")
+      .formParam("form_SUBMIT", "1")
+      .formParam("form%3A_link_hidden_", ""))
+
+    val finishApply =
+    exec(http("Dashboard - Apply for Role: Finish apply for Role without Approval")
+      .post("/goIdentity/jsf/2/workflow-custom/Dashboard/ApplyForRoleSelfService_3501_1/ApplyForRoleSelfService_3501_1Init.jsp?form:wizardApplyForRoleSelfService_finish=Apply")
+      .headers(headers_0)
+      .formParam("form_SUBMIT", "1")
+      .formParam("form%3A_link_hidden_", ""))
+  }
 }
